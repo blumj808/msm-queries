656d79fb3071a34fa8f73be668c85ecffb942ee0
blumj808 <blumenfeld.jake@gmail.com>
2023-11-10 13:29:03 +0000

    save

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,7 +5,7 @@ render({ :template => "director_templates/list"})
 
   def show
     the_id = params.fetch("the_id")
-    matching_records = Director.where({id => the_id })
+    matching_records = Director.where({:id => the_id })
     
     @the_director = matching_records.at(0)
     
